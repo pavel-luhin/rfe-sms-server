93f49aebadfa7c77d3e193c0f449e8d87206eec4
Pavel Luhin <pavel.luhin@gmail.com>
2016-07-08 14:49:29 +0300

    Logging exteptions on sms type

--- src/main/java/by/bsu/rfe/smsservice/controller/SMSController.java
+++ src/main/java/by/bsu/rfe/smsservice/controller/SMSController.java
@@ -1,23 +1,22 @@
 package by.bsu.rfe.smsservice.controller;
 
-import by.bsu.rfe.smsservice.common.dto.SMSResultDTO;
-import by.bsu.rfe.smsservice.common.entity.SmsTemplateEntity;
-import by.bsu.rfe.smsservice.common.sms.SmsDTO;
-import by.bsu.rfe.smsservice.service.SmsTemplateService;
-import by.bsu.rfe.smsservice.service.WebSMSService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+
+import by.bsu.rfe.smsservice.common.dto.SMSResultDTO;
+import by.bsu.rfe.smsservice.common.entity.SmsTemplateEntity;
+import by.bsu.rfe.smsservice.common.sms.SmsDTO;
+import by.bsu.rfe.smsservice.service.SmsTemplateService;
+import by.bsu.rfe.smsservice.service.WebSMSService;
 
 /**
  * Created by pluhin on 12/27/15.
@@ -25,6 +24,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping(value = "/sms")
 public class SMSController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SMSController.class);
 
     @Autowired
     private WebSMSService webSMSService;
@@ -45,6 +46,7 @@ public class SMSController {
         if (smsTemplate != null) {
             smsDTO.setSmsTemplate(smsTemplate);
         } else {
+            LOGGER.error("Invalid send SMS URI path {}. Could not detect correct SMS type.", requestUri);
             throw new IllegalArgumentException("Invalid send SMS URI path. Could not detect correct SMS type.");
         }
     }
