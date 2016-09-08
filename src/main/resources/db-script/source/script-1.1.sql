d93eb1646d27e23cb4f68d4474fa01bebe86128f
Pavel Luhin <pavel.luhin@gmail.com>
2016-09-08 18:42:41 +0300

    Update foreign keys in script

--- src/main/resources/db-script/source/script-1.1.sql
+++ src/main/resources/db-script/source/script-1.1.sql
@@ -1,7 +1,9 @@
+ALTER DATABASE CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `credentials` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
-  `sender` varchar(255) DEFAULT NULL,
+  `sender` varchar(255) DEFAULT NULL UNIQUE,
   `api_key` varchar(255) DEFAULT NULL,
   `created_by` varchar(255) NOT NULL,
   `created_date` datetime NOT NULL,
@@ -10,14 +12,14 @@ CREATE TABLE IF NOT EXISTS `credentials` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
   `default_credentials` int(11) DEFAULT NULL,
   `created_by` varchar(255) NOT NULL,
   `created_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `def_creds_key` (`default_credentials`),
-  CONSTRAINT `def_creds_key` FOREIGN KEY (`default_credentials`) REFERENCES `credentials` (`id`)
+  CONSTRAINT `def_creds_key` FOREIGN KEY (`default_credentials`) REFERENCES `credentials` (`id`) ON DELETE SET NULL
 );
 
 INSERT INTO `user` (username, password, created_by, created_date) VALUES ('pavel.luhin@gmail.com', 'q1w2e3r4t5', 'SYSTEM', NOW());
@@ -29,12 +31,12 @@ CREATE TABLE `authentication_token` (
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `token_user_fk` (`user_id`),
-  CONSTRAINT `token_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  CONSTRAINT `token_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `sms_template` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sms_type` varchar(255) NOT NULL,
+  `sms_type` varchar(255) NOT NULL UNIQUE,
   `template` longtext DEFAULT NULL,
   `uri_path` varchar(255) NOT NULL,
   `created_by` varchar(255) NOT NULL,
@@ -56,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `email_template` (
   `sms_template` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `template_fk` (`sms_template`),
-  CONSTRAINT `template_fk` FOREIGN KEY (`sms_template`) REFERENCES `sms_template` (`id`)
+  CONSTRAINT `template_fk` FOREIGN KEY (`sms_template`) REFERENCES `sms_template` (`id`) ON DELETE CASCADE
 );
 
 INSERT INTO `email_template` (subject, content, created_by, created_date, sms_template) VALUES ('Вас пригласили на собеседование',
@@ -72,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `external_application` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_by` varchar(255) NOT NULL,
   `created_date` datetime NOT NULL,
-  `application_name` varchar(255) NOT NULL,
+  `application_name` varchar(255) NOT NULL UNIQUE,
   `authentication` varchar(255) NOT NULL,
   `default_credentials` tinyblob,
   PRIMARY KEY (`id`)
@@ -83,8 +85,8 @@ CREATE TABLE `users_credentials` (
   `users_id` int(11) NOT NULL,
   KEY `user_user_fk` (`users_id`),
   KEY `user_cred_fk` (`credentials_id`),
-  CONSTRAINT `user_cred_fk` FOREIGN KEY (`credentials_id`) REFERENCES `credentials` (`id`),
-  CONSTRAINT `user_user_fk` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`)
+  CONSTRAINT `user_cred_fk` FOREIGN KEY (`credentials_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `user_user_fk` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `applications_credentials` (
@@ -92,8 +94,8 @@ CREATE TABLE `applications_credentials` (
   `applications_id` int(11) NOT NULL,
   KEY `app_app_fk` (`applications_id`),
   KEY `app_cred_fk` (`credentials_id`),
-  CONSTRAINT `app_app_fk` FOREIGN KEY (`applications_id`) REFERENCES `external_application` (`id`),
-  CONSTRAINT `app_cred_fk` FOREIGN KEY (`credentials_id`) REFERENCES `credentials` (`id`)
+  CONSTRAINT `app_app_fk` FOREIGN KEY (`applications_id`) REFERENCES `external_application` (`id`) ON DELETE CASCADE ,
+  CONSTRAINT `app_cred_fk` FOREIGN KEY (`credentials_id`) REFERENCES `credentials` (`id`) ON DELETE CASCADE
 ) ;
 
 CREATE TABLE `person` (
@@ -120,8 +122,8 @@ CREATE TABLE `persons_have_groups` (
   `group_id` int(11) NOT NULL,
   KEY `group_fk` (`group_id`),
   KEY `person_fk` (`person_id`),
-  CONSTRAINT `person_fk` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`),
-  CONSTRAINT `group_fk` FOREIGN KEY (`group_id`) REFERENCES `recipient_group` (`id`)
+  CONSTRAINT `person_fk` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`) ON DELETE CASCADE ,
+  CONSTRAINT `group_fk` FOREIGN KEY (`group_id`) REFERENCES `recipient_group` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `statistics` (
