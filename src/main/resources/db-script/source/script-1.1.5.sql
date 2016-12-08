5f4b71caeaa2911ab04a821944f13d8ba5fef0a7
Pavel Luhin <pavel.luhin@gmail.com>
2016-12-08 19:20:01 +0300

    Change SMS type in script

--- src/main/resources/db-script/source/script-1.1.5.sql
+++ src/main/resources/db-script/source/script-1.1.5.sql
@@ -3,15 +3,8 @@ SET
   `template` = '${STUDENT_FIRST_NAME}, ${COMPANY_NAME} приглашает Вас на собеседование ${INTERVIEW_TIME} по адресу ${COMPANY_ADDRESS}'
 WHERE
   `sms_type` = 'InterviewSMS';
-INSERT INTO `email_template` (subject, content, created_by, created_date, sms_template) VALUES (
-  'Вас пригласили на собеседование',
-  'Здравствуйте, ${STUDENT_FIRST_NAME} ${STUDENT_LAST_NAME},<br>Компания ${COMPANY_NAME}
-  приглашает Вас на собеседование ${INTERVIEW_DATE} по адресу ${COMPANY_ADDRESS}',
-  'SYSTEM',
-  NOW(),
-  LAST_INSERT_ID());
 
-SET @sms_template_id = (SELECT id FROM `sms_template` WHERE `sms_type` = 'RegisterUserSMS');
+SET @sms_template_id = (SELECT id FROM `sms_template` WHERE `sms_type` = 'InterviewSMS');
 
 UPDATE `email_template`
 SET
