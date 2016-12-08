daf163996b6779ce52612035bbabae6e73ff3e7e
Pavel Luhin <pavel.luhin@gmail.com>
2016-12-08 19:08:15 +0300

    Update interview email template

--- src/main/resources/db-script/source/script-1.1.5.sql
+++ src/main/resources/db-script/source/script-1.1.5.sql
@@ -1,9 +1,20 @@
-INSERT INTO `sms_template` (sms_type, template, uri_path, created_by, created_date) VALUES
-  ('InterviewSMS',
-   '${STUDENT_FIRST_NAME}, ${COMPANY_NAME} приглашает Вас на собеседование ${INTERVIEW_DATE} по адресу ${INTERVIEW_TIME}',
-   '/rest/sms/interview', 'SYSTEM', NOW());
 UPDATE `sms_template`
 SET
   `template` = '${STUDENT_FIRST_NAME}, ${COMPANY_NAME} приглашает Вас на собеседование ${INTERVIEW_TIME} по адресу ${COMPANY_ADDRESS}'
 WHERE
   `sms_type` = 'InterviewSMS';
+INSERT INTO `email_template` (subject, content, created_by, created_date, sms_template) VALUES (
+  'Вас пригласили на собеседование',
+  'Здравствуйте, ${STUDENT_FIRST_NAME} ${STUDENT_LAST_NAME},<br>Компания ${COMPANY_NAME}
+  приглашает Вас на собеседование ${INTERVIEW_DATE} по адресу ${COMPANY_ADDRESS}',
+  'SYSTEM',
+  NOW(),
+  LAST_INSERT_ID());
+
+SET @sms_template_id = (SELECT id FROM `sms_template` WHERE `sms_type` = 'RegisterUserSMS');
+
+UPDATE `email_template`
+SET
+  `content` = 'Здравствуйте, ${STUDENT_FIRST_NAME} ${STUDENT_LAST_NAME},<br>Компания ${COMPANY_NAME} приглашает Вас на собеседование ${INTERVIEW_TIME} по адресу ${COMPANY_ADDRESS}'
+WHERE
+  `sms_template` = @sms_template_id;
