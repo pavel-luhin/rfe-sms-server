5fb9c9659f5b0da2b621144acfa3a5c4787ff943
Pavel Luhin <pavel.luhin@gmail.com>
2018-01-23 18:35:05 +0300

    Use java config instead of xml

---
+++ src/main/java/by/bsu/rfe/smsservice/config/SchedulerConfiguration.java
@@ -0,0 +1,17 @@
+package by.bsu.rfe.smsservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@Configuration
+@ComponentScan("by.bsu.rfe.smsservice.scheduler.**")
+public class SchedulerConfiguration {
+
+  @Bean
+  public SchedulerFactoryBean schedulerFactoryBean() {
+    return new SchedulerFactoryBean();
+  }
+
+}
--- src/main/java/by/bsu/rfe/smsservice/security/filter/AuthenticationTokenFilter.java
+++ src/main/java/by/bsu/rfe/smsservice/security/filter/AuthenticationTokenFilter.java
@@ -2,51 +2,46 @@ package by.bsu.rfe.smsservice.security.filter;
 
 import by.bsu.rfe.smsservice.security.helper.AuthenticationHelper;
 import by.bsu.rfe.smsservice.security.util.SecurityUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
-
+import java.io.IOException;
+import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
 
 /**
  * Created by pluhin on 3/21/16.
  */
 public class AuthenticationTokenFilter extends GenericFilterBean {
 
-    @Autowired
-    private List<AuthenticationHelper> authenticationHelpers;
+  @Autowired
+  private List<AuthenticationHelper> authenticationHelpers;
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        if (servletRequest instanceof HttpServletRequest) {
-            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+    if (servletRequest instanceof HttpServletRequest) {
+      HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
 
-            String token = SecurityUtil.getUserAuthToken(httpServletRequest);
+      String token = SecurityUtil.getUserAuthToken(httpServletRequest);
 
-            Authentication authentication = null;
+      Authentication authentication = null;
 
-            for (AuthenticationHelper helper : authenticationHelpers) {
-                authentication = helper.tryWith(token);
+      for (AuthenticationHelper helper : authenticationHelpers) {
+        authentication = helper.tryWith(token);
 
-                if (authentication != null) {
-                    break;
-                }
-            }
-
-            if (authentication == null) {
-                throw new BadCredentialsException("Invalid access token");
-            }
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (authentication != null) {
+          break;
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+      }
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
 }
