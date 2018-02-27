fadebcb85cd20ad48b5a6c1143aaced0eb37b6e7
Pavel Luhin <pavel.luhin@gmail.com>
2018-02-27 10:51:25 +0300

    Refactor statistics controller

--- src/main/java/by/bsu/rfe/smsservice/service/StatisticsService.java
+++ src/main/java/by/bsu/rfe/smsservice/service/StatisticsService.java
@@ -1,6 +1,7 @@
 package by.bsu.rfe.smsservice.service;
 
 import by.bsu.rfe.smsservice.common.dto.StatisticsDTO;
+import by.bsu.rfe.smsservice.common.dto.page.PageRequestDTO;
 import by.bsu.rfe.smsservice.common.dto.page.PageResponseDTO;
 import by.bsu.rfe.smsservice.common.entity.StatisticsEntity;
 import java.util.List;
@@ -14,7 +15,7 @@ public interface StatisticsService {
 
   void saveStatistics(StatisticsEntity statisticsEntity);
 
-  PageResponseDTO getStatisticsPage(int skip, int offset, String sortField, String sortDirection);
+  PageResponseDTO getStatisticsPage(PageRequestDTO requestDTO);
 
   Long count();
 }
--- src/main/java/by/bsu/rfe/smsservice/service/impl/StatisticsServiceImpl.java
+++ src/main/java/by/bsu/rfe/smsservice/service/impl/StatisticsServiceImpl.java
@@ -1,18 +1,18 @@
 package by.bsu.rfe.smsservice.service.impl;
 
 import by.bsu.rfe.smsservice.common.dto.StatisticsDTO;
+import by.bsu.rfe.smsservice.common.dto.page.PageRequestDTO;
 import by.bsu.rfe.smsservice.common.dto.page.PageResponseDTO;
 import by.bsu.rfe.smsservice.common.entity.StatisticsEntity;
-import by.bsu.rfe.smsservice.common.pagination.ChunkRequest;
 import by.bsu.rfe.smsservice.repository.StatisticsRepository;
 import by.bsu.rfe.smsservice.service.StatisticsService;
 import by.bsu.rfe.smsservice.util.DozerUtil;
+import by.bsu.rfe.smsservice.util.PageUtil;
 import java.util.List;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 /**
@@ -32,10 +32,9 @@ public class StatisticsServiceImpl implements StatisticsService {
     return DozerUtil.mapList(mapper, statisticsEntities, StatisticsDTO.class);
   }
 
-  public PageResponseDTO getStatisticsPage(int skip, int offset, String sortField,
-      String sortDirection) {
-    Sort sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection), sortField));
-    Pageable pageable = new ChunkRequest(skip, offset, sort);
+  public PageResponseDTO getStatisticsPage(PageRequestDTO requestDTO) {
+    Pageable pageable = PageUtil.createPage(requestDTO);
+
     Page<StatisticsEntity> entities = statisticsRepository.findAll(pageable);
     return new PageResponseDTO<>(
         DozerUtil.mapList(mapper, entities.getContent(), StatisticsDTO.class), count());
