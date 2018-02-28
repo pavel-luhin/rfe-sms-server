6cdf10aa2eef1dae60846949482b76f144662eed
Pavel Luhin <pavel.luhin@gmail.com>
2018-02-28 17:44:52 +0300

    Add missing autowired

--- src/main/java/by/bsu/rfe/smsservice/service/impl/AuthenticationTokenServiceImpl.java
+++ src/main/java/by/bsu/rfe/smsservice/service/impl/AuthenticationTokenServiceImpl.java
@@ -4,11 +4,13 @@ import by.bsu.rfe.smsservice.common.entity.AuthenticationTokenEntity;
 import by.bsu.rfe.smsservice.repository.AuthenticationTokenRepository;
 import by.bsu.rfe.smsservice.service.AuthenticationTokenService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationTokenServiceImpl implements AuthenticationTokenService {
 
+  @Autowired
   private AuthenticationTokenRepository tokenRepository;
 
   @Override
