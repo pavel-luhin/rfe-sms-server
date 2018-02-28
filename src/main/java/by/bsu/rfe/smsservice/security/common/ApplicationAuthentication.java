8ec43a8df74d54127d7dd7f635623eec7209c619
Pavel Luhin <pavel.luhin@gmail.com>
2018-02-28 12:50:16 +0300

    Format code

--- src/main/java/by/bsu/rfe/smsservice/security/common/ApplicationAuthentication.java
+++ src/main/java/by/bsu/rfe/smsservice/security/common/ApplicationAuthentication.java
@@ -1,60 +1,59 @@
 package by.bsu.rfe.smsservice.security.common;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-
 import java.util.ArrayList;
 import java.util.Collection;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 /**
  * Created by pluhin on 7/12/17.
  */
 public class ApplicationAuthentication implements Authentication {
 
-    private String applicationName;
+  private String applicationName;
 
-    private String token;
+  private String token;
 
-    private boolean authenticated;
+  private boolean authenticated;
 
-    public ApplicationAuthentication(String applicationName, String token) {
-        this.applicationName = applicationName;
-        this.token = token;
-        this.authenticated = true;
-    }
+  public ApplicationAuthentication(String applicationName, String token) {
+    this.applicationName = applicationName;
+    this.token = token;
+    this.authenticated = true;
+  }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>();
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return new ArrayList<>();
+  }
 
-    @Override
-    public Object getCredentials() {
-        return token;
-    }
+  @Override
+  public Object getCredentials() {
+    return token;
+  }
 
-    @Override
-    public Object getDetails() {
-        return null;
-    }
+  @Override
+  public Object getDetails() {
+    return null;
+  }
 
-    @Override
-    public Object getPrincipal() {
-        return applicationName;
-    }
+  @Override
+  public Object getPrincipal() {
+    return applicationName;
+  }
 
-    @Override
-    public boolean isAuthenticated() {
-        return authenticated;
-    }
+  @Override
+  public boolean isAuthenticated() {
+    return authenticated;
+  }
 
-    @Override
-    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-        this.authenticated = isAuthenticated;
-    }
+  @Override
+  public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+    this.authenticated = isAuthenticated;
+  }
 
-    @Override
-    public String getName() {
-        return applicationName;
-    }
+  @Override
+  public String getName() {
+    return applicationName;
+  }
 }
