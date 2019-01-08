41722d8e31f00cf5d8dbd444a0993fb4a58f1025
Pavel Luhin <pavel.luhin@moneyman.ru>
2019-01-08 21:39:17 +0300

    REfactor parameters collector

--- sms-server-api/src/main/java/by/bsu/rfe/smsservice/builder/parameters/ParametersCollectorResolver.java
+++ sms-server-api/src/main/java/by/bsu/rfe/smsservice/builder/parameters/ParametersCollectorResolver.java
@@ -4,29 +4,27 @@ import by.bsu.rfe.smsservice.builder.parameters.impl.GroupParametersCollector;
 import by.bsu.rfe.smsservice.builder.parameters.impl.NumberParametersCollector;
 import by.bsu.rfe.smsservice.builder.parameters.impl.PersonParametersCollector;
 import by.bsu.rfe.smsservice.common.enums.RecipientType;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ParametersCollectorResolver {
 
-  @Autowired
-  private NumberParametersCollector numberParametersCollector;
+  private final Map<RecipientType, ParametersCollector> dictionary = new HashMap<>();
 
   @Autowired
-  private PersonParametersCollector personParametersCollector;
-
-  @Autowired
-  private GroupParametersCollector groupParametersCollector;
-
-  public ParametersCollector resolve(RecipientType type) {
-    if (type == RecipientType.NUMBER) {
-      return numberParametersCollector;
-    } else if (type == RecipientType.PERSON) {
-      return personParametersCollector;
-    } else {
-      return groupParametersCollector;
-    }
+  public ParametersCollectorResolver(
+      NumberParametersCollector numberParametersCollector,
+      PersonParametersCollector personParametersCollector,
+      GroupParametersCollector groupParametersCollector) {
+    dictionary.put(RecipientType.GROUP, groupParametersCollector);
+    dictionary.put(RecipientType.PERSON, personParametersCollector);
+    dictionary.put(RecipientType.NUMBER, numberParametersCollector);
   }
 
+  public ParametersCollector resolve(RecipientType type) {
+    return dictionary.get(type);
+  }
 }
